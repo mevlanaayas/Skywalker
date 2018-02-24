35c4950dd66acaafe73a70c8f86d6df01bbde400
mevlanaayas <mevlanaayas@gmail.com>
2018-02-25 00:37:23 +0300

    Reversed environment variables

--- Skywalker/settings.py
+++ Skywalker/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from Skywalker.environment import SECRET_KEY as SK
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SK
+SECRET_KEY = os.environ['SK']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
