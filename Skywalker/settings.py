0acdff38dadd37f055058df8be9ff3d98c50da16
mevlanaayas <mevlanaayas@gmail.com>
2018-02-25 00:29:30 +0300

    Added environment variables

--- Skywalker/settings.py
+++ Skywalker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from Skywalker.environment import SECRET_KEY as SK
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=@%vt1lr$+q9e^(u(k1^^*mg+*u3@gn4j@_rao0x%(n!^($#*3'
+SECRET_KEY = SK
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
