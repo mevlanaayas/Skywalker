75ff9779ddd4298f26c6a25b6420e4f165e97348
mevlanaayas <mevlanaayas@gmail.com>
2018-03-23 23:30:02 +0300

    Added label data field to map model. Serializer is needed to change

--- base/Rest/serializers/map.py
+++ base/Rest/serializers/map.py
@@ -9,7 +9,7 @@ class MapSerializer(ModelSerializer):
 
     """
     labels = SerializerMethodField()
-    label_data = serializers.CharField(write_only=True)
+    # label_data = serializers.CharField(write_only=True)
 
     @staticmethod
     def get_labels(obj):
@@ -22,7 +22,9 @@ class MapSerializer(ModelSerializer):
         model = Map
         fields = ('id', 'name', 'labels', 'movement_data', 'map_data', 'label_data')
 
+    """
     def create(self, validated_data):
         label_data = validated_data['label_data']
         del validated_data['label_data']
         return super(MapSerializer, self).create(validated_data)
+    """
